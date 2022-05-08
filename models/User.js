1e7887a3d1f581f447cc85485e9efad5d7936d9f
LisaCR01 <lisagunn01@gmail.com>
2022-05-08 15:26:30 +0100

    added index.js info to do with models

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./User');
+
+const userSchema = new Schema(
+    {
+     username: {
+        type: String,
+        required: true,
+        max_length: 50, 
+        trim:true,
+        unique: true, 
+      },
+      email: {
+        type: String,
+        required: true,
+        max_length: 50,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+           user: {
+             type: Schema.Types.ObjectId,
+             ref: 'Username',
+           },
+        }
+     ]
+    },
+    
+    {
+      toJSON: {
+        virtuals: true,
+        getters:true,
+      },
+    }
+  );
+  userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
